a5ad65006636d1bebf2280d2d70e48373e7530d2
hopper80 <hhchoi80@gmail.com>
2023-01-13 09:55:17 +0900

    Update

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-#nohup python3 main_mqtt_motion_face_v3.py HK_PiCam02 &
-nohup python3 main_mqtt_motion_face_v3.py HK_PiCam02 1>/dev/null 2>&1 &
+#nohup python3 main_mqtt_motion_face_v3.py HK_PiCam01 &
+nohup python3 main_mqtt_motion_face_v3.py HK_PiCam01 1>/dev/null 2>&1 &
